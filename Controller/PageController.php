1977a5e303f39ae6076e199281e33376dc57b496
pavel <p.petrov@akristo.com>
2014-01-18 11:59:40 +0200

    Get page by slug only by default locale slug.

--- Controller/PageController.php
+++ Controller/PageController.php
@@ -12,7 +12,7 @@ class PageController extends Controller
         $request = $this->getRequest();
         $entity = $this->container->getParameter("cadrone.nice_pages.page_class");
 
-        $locale = (new Page)->getDefaultLocale();
+        $locale = (new $entity)->getDefaultLocale();
         $page = $this->getDoctrine()->getRepository($entity)->findPageBySlug($request->get("slug"), $locale);
 
         return $this->render("CadroneNicePagesBundle:Page:index.html.twig", array(
